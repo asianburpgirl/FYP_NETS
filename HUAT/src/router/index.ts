1374ed9eac3e8c81e94fcbaef68b1fee482a4279
asianburpgirl <estherrrlychee@gmail.com>
2023-01-12 16:30:33 +0800

    Added routing and navigation

--- HUAT/src/router/index.ts
+++ HUAT/src/router/index.ts
@@ -26,6 +26,14 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: 'tab3',
         component: () => import('@/views/Tab3Page.vue')
+      },
+      {
+        path: 'tab4',
+        component: () => import('@/views/Tab4Page.vue')
+      },
+      {
+        path: 'tab5',
+        component: () => import('@/views/Tab5Page.vue')
       }
     ]
   }
--- HUAT/src/views/TabsPage.vue
+++ HUAT/src/views/TabsPage.vue
@@ -4,18 +4,28 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-icon :icon="home" />
+          <ion-label>Home</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-icon :icon="card" />
+          <ion-label>Wallet</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-icon :icon="albums" />
+          <ion-label>View Plans</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon :icon="map" />
+          <ion-label>Map</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="tab5" href="/tabs/tab5">
+          <ion-icon :icon="person" />
+          <ion-label>My Profile</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,5 +34,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { albums, card, ellipse, home, map, person, square, triangle } from 'ionicons/icons';
 </script>
