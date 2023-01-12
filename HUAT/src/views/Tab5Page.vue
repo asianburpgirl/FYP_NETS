12ed9daa5f07156f169a2cbb097f58ab80d79e6c
NikkiCJQ <89062324+NikkiCJQ@users.noreply.github.com>
2023-01-13 01:14:09 +0800

    nearby me page

--- HUAT/src/views/Tab5Page.vue
+++ HUAT/src/views/Tab5Page.vue
@@ -1,23 +1,48 @@
-<template>
+  <template>
     <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Tab 5</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content :fullscreen="true">
-        <ion-header collapse="condense">
+      <ion-content>
+        <ion-header>
           <ion-toolbar>
-            <ion-title size="large">Tab 5</ion-title>
+            <ion-title size="large" class="ion-text-center">Nearby Me</ion-title>
           </ion-toolbar>
         </ion-header>
-        <!-- <ExploreContainer name="Tab 5 page" /> -->
+        <ion-searchbar></ion-searchbar>
+        <ion-card>
+          <ion-img src="assets/images/ion.jpg"></ion-img>
+            <ion-card-header>
+              <ion-card-title>Ion Orchard</ion-card-title>
+              <ion-card-subtitle>400 lots Left</ion-card-subtitle>
+            </ion-card-header>
+        </ion-card>
+        <ion-card>
+          <ion-img src="assets/images/wisma.jpeg"></ion-img>
+            <ion-card-header>
+              <ion-card-title>Wisma atria</ion-card-title>
+              <ion-card-subtitle>230 lots Left</ion-card-subtitle>
+            </ion-card-header>
+        </ion-card>
       </ion-content>
     </ion-page>
   </template>
+  <ion-content class="ion-padding"></ion-content>
   
-  <script setup lang="ts">
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-//   import ExploreContainer from '@/components/ExploreContainer.vue';
+  <script lang="ts">
+    import { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSearchbar} from '@ionic/vue';
+    import { defineComponent } from 'vue';
+  
+    export default defineComponent({
+      components: { IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSearchbar },
+    });
   </script>
   
+  <style scoped>
+    /* iOS places the subtitle above the title */
+    ion-card-header.ios {
+      display: flex;
+      flex-flow: column-reverse;
+    }
+    ion-img::part(image) {
+      width: 100%; /* or whatever value */
+      /* you can set the height value too */
+    }
+  </style>
