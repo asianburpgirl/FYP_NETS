ab17dcb70b97213ea9a14ff066e87281ab546a55
asianburpgirl <estherrrlychee@gmail.com>
2023-01-16 17:59:47 +0800

    Removed additional icon on bottom nav

--- HUAT/src/App.vue
+++ HUAT/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <ion-app>
-    <ion-router-outlet />
-  </ion-app>
+  <ion-page>
+    <ion-app>
+      <ion-router-outlet></ion-router-outlet>
+    </ion-app>
+  </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { albums, card, ellipse, home, map, person, square, triangle } from 'ionicons/icons';
 </script>
--- HUAT/src/views/TabsPage.vue
+++ HUAT/src/views/TabsPage.vue
@@ -27,13 +27,6 @@
           <ion-icon :icon="person" />
           <ion-label>My Profile</ion-label>
         </ion-tab-button>
-
-        <!-- for viewBooking page Navbar -->
-        <ion-tab-button tab="viewBookingtab5" href="/viewBooking">
-          <ion-icon :icon="person" />
-          <ion-label>My Profile</ion-label>
-        </ion-tab-button>
-        
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
