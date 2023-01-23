07c097d56a25f968dbdc003b7bcf9f8c7933c12b
minthukha99 <minthukha99@gmail.com>
2023-01-23 14:48:23 +0800

    init hard coded map

--- HUAT/src/views/mapPage.vue
+++ HUAT/src/views/mapPage.vue
@@ -1,16 +1,40 @@
 <template>
-  <base-layout pageTitle="Map Page" needToolBar="y"> 
-  
+  <base-layout pageTitle="Map" needToolBar="y">
+    <GMapMap
+      :center="center"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 400px; height: 800px"
+    >
+      <GMapCluster>
+        <!-- <GMapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center = m.position"
+      /> -->
+        <GMapMarker />
+      </GMapCluster>
+    </GMapMap>
   </base-layout>
 </template>
-
-<script setup lang="ts">
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-} from "@ionic/vue";
-//   import ExploreContainer from '@/components/ExploreContainer.vue';
+<script>
+export default {
+  name: "MapPage",
+  data() {
+    return {
+      center: { lat: 51.093048, lng: 6.84212 },
+      markers: [
+        {
+          position: {
+            lat: 51.093048,
+            lng: 6.84212,
+          },
+        }, // Along list of clusters
+      ],
+    };
+  },
+};
 </script>
