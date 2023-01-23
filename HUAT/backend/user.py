69078dbf35aa0fbb8578ac73ac93a3780c5ae8f2
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2023-01-23 13:44:26 +0800

    Update user.py

--- HUAT/backend/user.py
+++ HUAT/backend/user.py
@@ -5,8 +5,8 @@ from itsdangerous import json
 from os import environ
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/localconnect'
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root:root@localhost:8889/localconnect'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/localconnect'
+# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root:root@localhost:8889/localconnect'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
@@ -64,6 +64,33 @@ def get_all():
         }
     ), 404
 
+#authenticate user
+@app.route("/userlogin", methods=["POST"])
+def login_by_username():
+    if request.is_json:
+        user_details = request.get_json()
+        username = user_details["username"]
+        password = user_details["password"]
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if user.password == password:
+                return jsonify(
+                    {
+                        "code": 200,
+                        "data": json.loads(
+                            json.dumps(user.json())
+                        ),
+                    }
+                )
+            else:
+                return (
+                    jsonify({"code": 403, "message": "Wrong password."}),
+                    403,
+                ) 
+        return jsonify({"code": 404, "message": "Customer not found."}), 404
+    else:
+        return jsonify({"code": 500, "message": "Input is not JSON."}), 500
+
 # create user
 @app.route("/users/<int:userID>" , methods = ['POST'])
 def createUser(userID):
@@ -97,7 +124,7 @@ def createUser(userID):
             "data": newUser.json(),
             "message": "Your account has been created"
         }
-    ), 201 
+    ), 201
 
 # update user password
 @app.route("/users/<int:userID>", methods = ["PUT"])
