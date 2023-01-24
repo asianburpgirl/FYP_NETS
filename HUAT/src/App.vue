4b65b4b82169ef2a8260df276dcc6e6843c29575
minthukha99 <minthukha99@gmail.com>
2023-01-24 23:05:37 +0800

    added markers for carparks

--- HUAT/src/App.vue
+++ HUAT/src/App.vue
@@ -7,6 +7,6 @@
 </template>
 
 <script setup lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { IonApp,IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { albums, card, ellipse, home, map, person, square, triangle } from 'ionicons/icons';
 </script>
