244598814ec1685f87375def1bfe86a142e363b7
minthukha99 <minthukha99@gmail.com>
2023-01-25 09:42:01 +0800

    map made with official GoogleMap

--- HUAT/src/views/mapPage.vue
+++ HUAT/src/views/mapPage.vue
@@ -1,144 +1,133 @@
 <template>
-  <base-layout pageTitle="Map" needToolBar="y">
-    <GMapMap
-      :center="center"
-      :zoom="10"
-      map-type-id="terrain"
-      style="width: 350px; height: 600px"
-    >
-      <GMapMarker
-        :key="index"
-        v-for="(marker, index) in markers"
-        :position="marker.position"
-        :clickable="true"
-        @click="openMarker(marker.id)"
-      >
-        <GMapInfoWindow
-          :closeclick="true"
-          @closeclick="openMarker(null)"
-          :opened="openedMarkerID === marker.id"
-        >
-          <div>
-            <h1>{{ marker.name }}</h1>
-            <h5>
-              {{ marker.location }}
-              Avaiable slots: {{ marker.carparkSlotsTotalAvailable }} / {{ marker.carparkSlotsTotal }}
-            </h5>
-          </div>
-        </GMapInfoWindow>
-      </GMapMarker>
-    </GMapMap>
-
-    <div>
-      <a> Longitude: {{ center.lng }} Latitude: {{ center.lat }} </a>
-    </div>
-  </base-layout>
+  <ion-page class="ion-padding">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Map</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <capacitor-google-map id="map"></capacitor-google-map>
+      <div>
+        <a>
+          Longitude: {{ currentLocation.lng }} Latitude:
+          {{ currentLocation.lat }}
+        </a>
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 
-<script lang="ts">
+<script>
+import { GoogleMap } from "@capacitor/google-maps";
 import { Geolocation } from "@capacitor/geolocation";
 
 import { defineComponent } from "vue";
-// import axios from "axios";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+
 
 export default defineComponent({
-  // lat: 1.4505518,
-  // lng: 103.825558, // my home
-
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+  },
   data() {
     return {
-      openedMarkerID: null,
-      center: { lat: 1.3521, lng: 103.8198 },
-      markers: [
-        {
-          id: 1,
-          name: "Plaza Sing",
-          location: "68 Orchard Rd, Singapore 238839",
-          carparkSlotsTotal: 44,
-          carparkSlotsTotalAvailable: 10,
-          position: {
-            lat: 1.3007033161990564,
-            lng: 103.84528924122294, // plaza sing
-            // 1.2958419970838684, 103.85841587741238 suntec
-          },
-        },
-        {
-          id: 2,
-          name: "Suntec",
-          location: "1 Raffles Blvd, Singapore 039593",
-          carparkSlotsTotal: 40,
-          carparkSlotsTotalAvailable: 15,
-          position: {
-            lat: 1.2958419970838684,
-            lng: 103.85841587741238, // suntec
-          },
-        },
-      ],
+      currentLocation: { lat: 1.3521, lng: 103.8198 },
     };
   },
+
   mounted() {
-    this.getLiveMap();
+    this.createMap();
+    this.getCurrentLocation();
   },
+
   methods: {
-    openMarker(id: any) {
-      this.openedMarkerID = id;
-    },
-    getLiveMap() {
+    getCurrentLocation() {
       const location = Geolocation.getCurrentPosition();
-      console.log(location);
       location
         .then((result) => {
-          this.center = {
+          this.currentLocation = {
             lat: result.coords.latitude,
             lng: result.coords.longitude,
           };
-
-          // this.latitude = result.coords.latitude;
-          // this.longitude = result.coords.longitude;
-          // console.log(this.latitude);
-          // console.log(this.longitude);
         })
         .catch((err) => {
           // console.log(err)
           return err;
         });
     },
-  },
+    async createMap() {
+      try {
+        const mapRef = document.getElementById("map");
 
-  // methods: {
-  //   async getStreetAddressFrom(lat: string, long: string) {
-  //     try {
-  //       const { data } = await axios.get(
-  //         "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
-  //           lat +
-  //           "," +
-  //           long +
-  //           "&key=AIzaSyAJXGx7T2ypt5Ew5-9SbDTWF9gqloQUJwI"
-  //       );
-  //       if (data.error_message) {
-  //         console.log(data.error_message);
-  //       } else {
-  //         this.address = data.results[0].formatted_address;
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   },
-  //   locatorButtonPressed() {
-  //     navigator.geolocation.getCurrentPosition(
-  //       (position) => {
-  //         this.getStreetAddressFrom(
-  //           position.coords.latitude,
-  //           position.coords.longitude
-  //         );
-  //       },
-  //       (error) => {
-  //         console.log(error.message);
-  //       }
-  //     );
-  //   },
-  // },
+      const newMap = await GoogleMap.create({
+        id: "my-map", // Unique identifier for this map instance
+        element: mapRef, // reference to the capacitor-google-map element
+        apiKey: "AIzaSyAJXGx7T2ypt5Ew5-9SbDTWF9gqloQUJwI", // Your Google Maps API Key
+        config: {
+          center: {
+            // The initial position to be rendered by the map
+            lat: 1.3521,
+            lng: 103.8198,
+          },
+          zoom: 10, // The initial zoom level to be rendered by the map
+        },
+      });
+      const markers = await newMap.addMarkers(
+        [
+          {
+            title: "Plaza Sing",
+            snippet: "68 Orchard Rd, Singapore 238839",
+            coordinate: {
+              lat: 1.3007033161990564,
+              lng: 103.84528924122294,
+            },
+          },
+
+          {
+            coordinate: {
+              lat: 1.2958419970838684,
+              lng: 103.85841587741238,
+            },
+          },
+        ]
+      );
+
+      }
+      catch (e) {
+        console.log(e)
+      }
+      
+      // const mapType = await newMap.setMapType('Normal');
+      // newMap.setMapType('Normal');
+
+      // await newMap.enableClustering();
+      // enableClustering() => Promise<void>
+      // await newMap.enableCurrentLocation(true);
+    },
+
+    async addListeners() {
+      await this.newMap.setOnMarkerClickListener((event) => {
+        console.log(event);
+      });
+    },
+  },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+capacitor-google-map {
+  display: inline-block;
+  width: 450px;
+  height: 600px;
+}
+</style>
