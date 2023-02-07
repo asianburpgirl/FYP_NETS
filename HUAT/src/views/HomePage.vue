4d00bb4da3d812406c86c637cee6e459da30b712
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2023-02-08 00:59:39 +0800

    Update HomePage.vue

--- HUAT/src/views/HomePage.vue
+++ HUAT/src/views/HomePage.vue
@@ -1,15 +1,28 @@
 <template>
-  <base-layout :pageTitle="`Welcome, ${userData.name}!`" needToolBar="y">
+  <base-layout needToolBar="y">
     <div>
-      <h2>Percentage of bookings according to locations</h2>
-      <GChart type="PieChart" :options="PieChartOptions" :data="PieChartData" /> 
-      <GChart type="ColumnChart" :data="ColumnChartData" :options="ColumnChartOptions" />
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            <h2>Percentage of bookings per location</h2>
+            <GChart type="PieChart" :options="PieChartoptions" :data="PieChartData" />
+          </ion-col>
+          <ion-col>
+            <h2>Number of bookings per location</h2>
+            <GChart type="BarChart" :data="BarChartData" :options="BarChartOptions" />
+          </ion-col>
+          <ion-col>
+            <h2>Number of bookings per subscription</h2> 
+            <GChart type="ColumnChart" :data="ColumnChartData" :options="ColumnChartOptions" />
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </div>
   </base-layout>
 </template>
 
 <script lang="ts">
-import { GChart } from "vue-google-charts";
+import { GChart } from 'vue-google-charts';
 import { defineComponent } from "vue";
 import axios from 'axios';
 
@@ -23,28 +36,35 @@ export default defineComponent({
       PieChartData: [
         ['Bookings', 'Percentage of bookings']
       ],
+      BarChartData: [
+        ['Booking Location', 'No. of Bookings']
+      ],
       ColumnChartData: [
-        ['Year', 'Season Parking', 'Hourly Parking'],
-        ['2014', 1000, 400],
-        ['2015', 1170, 460],
-        ['2016', 660, 1120],
-        ['2017', 1030, 540],
+        ['Carpark Name', 'Subscription Plan', 'No. of Subscribers']
       ],
       PieChartoptions: {
         chart: {
-          title: "Percentage of bookings according to locations",
+          title: "Percentage of bookings per location",
           pieHole: 0.4,
         },
-        width: 1000,
-        height: 400
+        // width: 400,
+        // height: 400
+      },
+      BarChartOptions: {
+        chart: {
+          title: "Number of bookings per location",
+          pieHole: 0.4,
+        },
+        // width: 400,
+        // height: 400
       },
       ColumnChartOptions: {
         chart: {
-          title: "Percentage of bookings according to locations",
+          title: "Number of bookings per subscription",
           pieHole: 0.4,
         },
-        width: 1000,
-        height: 400
+        // width: 400,
+        // height: 400
       },
       userData: {}
     }
@@ -53,29 +73,64 @@ export default defineComponent({
     loadUserData() {
       this.userData = JSON.parse(localStorage.getItem("userData") || '{}');
     },
-    getCharts() {
-      let bookingData = [];
+    getPieChart() {
+      let pieData = [];
 
       const url = "http://localhost:5001/bookings";
       axios.get(url)
       .then(response => {
-        bookingData = response.data.data.bookings;
-        // console.log(bookingData)
-        for (let i = 0; i < bookingData.length; i ++) {
-          this.PieChartData.push([bookingData[i]['bookingLocation'],bookingData[i]['bookingID']])
+        pieData = response.data.data.bookings;
+        for (let i = 0; i < pieData.length; i ++) {
+          this.PieChartData.push([pieData[i]['bookingLocation'],pieData[i]['bookingID']])
         }
-        return bookingData
+        return pieData
         // console.log(response.data.data.bookings)
-        })
+      })
       .catch(error => {
         console.log(error.message)
-        })
-    }
+      })
+    },
+    getBarChart() {
+      let barData = [];
+      const url = "http://localhost:5003/carparks";
+
+      axios.get(url)
+      .then(response => {
+        barData = response.data.data.carparks;
+        for(let i = 0; i < barData.length; i++) {
+          this.BarChartData.push([barData[i]['carparkName'],barData[i]['currentCapacity']])
+        }
+        return barData
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+    },
+    getColumnChart() {
+      let columnData = [];
+      const url = "http://localhost:5003/carparks";
+      
+      axios.get(url)
+      .then(response => {
+        columnData = response.data.data.carparks;
+        // console.log(columnData)
+        for (let i=0; i < columnData.length; i++) {
+          this.ColumnChartData.push([columnData[i]['carparkName'],parseInt(columnData[i]['hourlyweekdaypeak']),
+          parseInt(columnData[i]['hourlyweekdaynonpeak'])])
+        }
+        return columnData
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+    },
   },
   // Calls function on page load
   mounted(){
     this.loadUserData();
-    this.getCharts();
+    this.getBarChart();
+    this.getPieChart();
+    this.getColumnChart();
   }
 });
 </script>
