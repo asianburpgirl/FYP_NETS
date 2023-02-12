fafb3faeafc4a8c41bec1da270b558206c31494e
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2023-02-12 17:13:57 +0800

    Changes to payment page

--- HUAT/src/views/paymentPage.vue
+++ HUAT/src/views/paymentPage.vue
@@ -1,72 +1,43 @@
 <template>
-    <table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">Title</th>
-      <th scope="col">Author</th>
-      <th scope="col">Purchase Price</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Green eggs and ham</td>
-      <td>Dr. Seuss</td>
-      <td>$9.99</td>
-      <td>
-        <div class="btn-group" role="group">
-            <button type="button" class="btn btn-primary btn-sm" @click="purchaseBook()"> Purchase </button>
-        </div>
-      </td>
-    </tr>
-  </tbody>
-</table>
+  <div>
+    <stripe-checkout
+      ref="checkoutRef"
+      mode="payment"
+      :pk="publishableKey"
+      :line-items="lineItems"
+      :success-url="successURL"
+      :cancel-url="cancelURL"
+      @loading="v => loading = v"
+    />
+    <button @click="submit">Pay now!</button>
+  </div>
 </template>
 
-
 <script>
-import { defineComponent } from 'vue'
-
-
-export default defineComponent({
-    data() {
-        return {
-            stripe: null,
-        }
+import { StripeCheckout } from '@vue-stripe/vue-stripe';
+export default {
+  components: {
+    StripeCheckout,
+  },
+  data () {
+    this.publishableKey = process.env.STRIPE_PUBLISHABLE_KEY;
+    return {
+      loading: false,
+      lineItems: [
+        {
+          price: 'some-price-id', // The id of the one-time price you created in your Stripe dashboard
+          quantity: 1,
+        },
+      ],
+      successURL: 'your-success-url',
+      cancelURL: 'your-cancel-url',
+    };
+  },
+  methods: {
+    submit () {
+      // You will be redirected to Stripe's secure checkout page
+      this.$refs.checkoutRef.redirectToCheckout();
     },
-    methods: {
-        purchaseBook() {
-          fetch('http://localhost:4242/create-payment-intent', {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            
-            body: JSON.stringify({ item:123 }),
-          })
-          .then((result) => result.json())
-          .then((data) => {
-            // Redirect to Stripe Checkout
-            return this.stripe.redirectToCheckout({ sessionId: 123 });
-          })
-          .then((res) => {
-            console.log(res);
-          });
-        },
-        getStripePublishableKey() {
-            fetch('http://localhost:4242/config')
-            .then((result) => result.json())
-            .then((data) => {
-                // Initialize Stripe.js
-                this.stripe = Stripe(data.publicKey); // eslint-disable-line no-undef
-            });
-        },
-        created() {
-            this.getStripePublishableKey();
-        },
-        mounted(){
-          const recaptchaScript = document.createElement('script')
-          recaptchaScript.setAttribute('src', 'https://js.stripe.com/v3/')
-          document.head.appendChild(recaptchaScript)
-        }
-    }
-})
+  },
+};
 </script>
