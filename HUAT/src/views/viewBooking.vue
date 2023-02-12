9334a78a627406c64433739b652cdb01bbd68f2a
minthukha99 <minthukha99@gmail.com>
2023-02-13 01:25:04 +0800

    booking deletion confirmation

--- HUAT/src/views/viewBooking.vue
+++ HUAT/src/views/viewBooking.vue
@@ -18,16 +18,23 @@
                 </u></ion-card-subtitle
               >
               <ion-card-subtitle
-                >Booking Amount:  $
-                  {{ eachBooking.amount }}
-                </ion-card-subtitle
-              >
+                >Booking Amount: $
+                {{ eachBooking.amount }}
+              </ion-card-subtitle>
               <ion-row class="ion-padding-top ion-justify-content-center">
-        <ion-button shape="round" @click="editBooking()">Edit</ion-button>
-      </ion-row>
-       <ion-row class="ion-padding-top ion-justify-content-center">
-        <ion-button shape="round" @click="deleteBooking(eachBooking.bookingID)" color="danger" >Delete</ion-button>
-      </ion-row>
+                <ion-button shape="round" @click="editBooking()"
+                  >Edit</ion-button
+                >
+              </ion-row>
+              <ion-row class="ion-padding-top ion-justify-content-center">
+                <!-- <ion-button shape="round" @click="deleteBooking(eachBooking.bookingID)" color="danger" >Delete</ion-button> -->
+                <ion-button
+                  shape="round"
+                  @click="presentAlert(eachBooking.bookingID)"
+                  color="danger"
+                  >Delete</ion-button
+                >
+              </ion-row>
             </ion-col>
           </ion-row>
           <ion-row>
@@ -36,15 +43,19 @@
                 >Start Date: {{ eachBooking.startDate }}</ion-card-subtitle
               >
               <br />
-     
-              <ion-card-subtitle>End Date: {{ eachBooking.endDate }}</ion-card-subtitle>
+
+              <ion-card-subtitle
+                >End Date: {{ eachBooking.endDate }}</ion-card-subtitle
+              >
             </ion-col>
             <ion-col>
               <ion-card-subtitle
                 >Start Time: {{ eachBooking.startTime }}</ion-card-subtitle
               >
               <br />
-              <ion-card-subtitle>End Time:{{ eachBooking.endTime }}</ion-card-subtitle>
+              <ion-card-subtitle
+                >End Time:{{ eachBooking.endTime }}</ion-card-subtitle
+              >
             </ion-col>
             <ion-col>
               <ion-item lines="none">
@@ -80,10 +91,12 @@ import {
   IonCardSubtitle,
   IonCardTitle,
   IonSearchbar,
-  IonBadge
+  IonBadge,
+  IonButton,
+  alertController,
 } from "@ionic/vue";
-import { defineComponent } from "vue";
-import { arrowBack, home, star } from "ionicons/icons";
+import { defineComponent, ref } from "vue";
+
 import axios from "axios";
 
 export default defineComponent({
@@ -93,28 +106,72 @@ export default defineComponent({
     IonCardSubtitle,
     IonCardTitle,
     IonSearchbar,
-    IonBadge
+    IonBadge,
+    IonButton,
   },
+
+  setup() {
+    const handlerMessage = ref("");
+    const presentAlert = async (bookingID) => {
+      const alert = await alertController.create({
+        header: "Are you sure you want to delete this booking?",
+        buttons: [
+          {
+            text: "Cancel",
+            cssClass: "alert-button-cancel",
+            role: "cancel",
+            handler: () => {
+              handlerMessage.value = "Alert canceled";
+            },
+          },
+          {
+            text: "Yes",
+            cssClass: "alert-button-confirm",
+            role: "confirm",
+            handler: () => {
+              handlerMessage.value = "Alert confirmed";
+              const url = "http://127.0.0.1:5001/bookings/"+bookingID; 
+              axios
+                .delete(url)
+                .then((response) => {
+                  console.log(response);
+                  location.reload();
+                })
+                .catch((error) => {
+                  console.log(error.message);
+                });
+            },
+          },
+        ],
+      });
+
+      await alert.present();
+      const { role } = await alert.onDidDismiss();
+    };
+    return {
+      presentAlert,
+    };
+  },
+
   data() {
     return {
       bookingDetails: [],
-      // passwordErrors: Array<string>(),
     };
   },
   methods: {
     deleteBooking(bookingID) {
+      console.log("HERE");
       const url = "http://127.0.0.1:5001/bookings/" + bookingID; // hardcoded
-      console.log(url)
+      console.log(url);
       axios
         .delete(url)
         .then((response) => {
-          console.log(response)
-        location.reload()
+          console.log(response);
+          location.reload();
         })
-       .catch((error) => {
+        .catch((error) => {
           console.log(error.message);
         });
-      
     },
     getAllBookings() {
       const url = "http://127.0.0.1:5001/bookings";
@@ -122,7 +179,7 @@ export default defineComponent({
         .get(url)
         .then((response) => {
           const data = response.data.data.bookings;
-          console.log(data)
+          console.log(data);
           for (const eachBooking of data) {
             const startDateTime = new Date(eachBooking.bookingStartDateTime);
             const startDateOnly =
@@ -133,7 +190,10 @@ export default defineComponent({
               "/" +
               startDateTime.getFullYear();
             const startTimeOnly =
-              startDateTime.getHours() - 8 + ":" + ('0' + startDateTime.getMinutes()).slice(-2);
+              startDateTime.getHours() -
+              8 +
+              ":" +
+              ("0" + startDateTime.getMinutes()).slice(-2);
 
             const endDateTime = new Date(eachBooking.bookingEndDateTime);
             const endDateOnly =
@@ -144,8 +204,10 @@ export default defineComponent({
               "/" +
               endDateTime.getFullYear();
 
-            const endTimeOnly =  ('0'+(endDateTime.getHours()-8).toString()).slice(-2)+ ":" + ('0' + endDateTime.getMinutes()).slice(-2);
-                     
+            const endTimeOnly =
+              ("0" + (endDateTime.getHours() - 8).toString()).slice(-2) +
+              ":" +
+              ("0" + endDateTime.getMinutes()).slice(-2);
 
             this.bookingDetails.push({
               bookingDate: eachBooking.bookingDate,
@@ -162,7 +224,6 @@ export default defineComponent({
               userID: eachBooking.userID,
             });
           }
-        
         })
         .catch((error) => {
           console.log(error.message);
@@ -188,5 +249,25 @@ ion-img::part(image) {
   width: 100%;
   /* or whatever value */
   /* you can set the height value too */
+}
+
+button.alert-button.alert-button-confirm {
+  background-color: var(--ion-color-success);
+  color: var(--ion-color-success-contrast);
+}
+
+.md button.alert-button.alert-button-confirm {
+  border-radius: 4px;
+}
+
+.ios button.alert-button.alert-button-confirm {
+  border-bottom-right-radius: 13px;
+  border-top-right-radius: 13px;
+}
+
+.ios button.alert-button.alert-button-cancel {
+  border-right: 0;
+  border-bottom-left-radius: 13px;
+  border-top-left-radius: 13px;
 }
 </style>
