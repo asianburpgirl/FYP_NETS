b93a740025e4c4241c3a43cec8726d287bd59d71
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2023-02-22 03:04:28 +0800

    Update successPage.vue

--- HUAT/src/views/successPage.vue
+++ HUAT/src/views/successPage.vue
@@ -1,3 +1,10 @@
 <template>
-    <p></p>
+    <ion-page class="ion-padding">
+    <ion-content class="ion-padding">
+      <p>Payment has been made. Thank you</p>
+    </ion-content>
+  </ion-page>
 </template>
+
+<style>
+</style>
