7fe5182e563622ed15ca528e2575443494914f5b
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2023-02-22 03:04:26 +0800

    Update paymentPage.vue

--- HUAT/src/views/paymentPage.vue
+++ HUAT/src/views/paymentPage.vue
@@ -1,71 +1,96 @@
 <template>
-  <table class="table table-hover">
-<thead>
-  <tr>
-    <th scope="col">Title</th>
-    <th scope="col">Author</th>
-    <th scope="col">Purchase Price</th>
-    <th></th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <td>Green eggs and ham</td>
-    <td>Dr. Seuss</td>
-    <td>$9.99</td>
-    <td>
-      <div class="btn-group" role="group">
-          <button type="button" class="btn btn-primary btn-sm" @click="purchaseBook()"> Purchase </button>
-      </div>
-    </td>
-  </tr>
-</tbody>
-</table>
+  <ion-page class="ion-padding">
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-back-button defaultHref="/tabs/wallet"></ion-back-button>
+        </ion-buttons>
+        <ion-title class="ion-text-center">Payment</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content class="ion-padding">
+      <ion-button shape="round" expand="block" size="large" @click="routeNets('Nets')">NETS</ion-button>
+      <ion-button shape="round" expand="block" size="large" @click="routeStripe()">Stripe</ion-button>
+    </ion-content>
+  </ion-page>
 </template>
 
 
 <script>
+import { 
+  IonButton, 
+  IonPage,
+  IonHeader,
+  IonTitle,
+  IonContent,
+  IonToolbar,
+  IonBackButton,
+} from '@ionic/vue';
 import { defineComponent } from 'vue'
+import axios from "axios";
 
 export default defineComponent({
+  components: { 
+    IonButton,
+    IonBackButton,
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+  },
   data() {
-      return {
-          stripe: null,
-      }
+    return {
+      stripe: null,
+      balance: 0,
+      userData: {}
+    }
   },
   methods: {
-      purchaseBook() {
-        fetch('http://localhost:4242/create-payment-intent', {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          
-          body: JSON.stringify({ item:123 }),
-        })
-        .then((result) => result.json())
-        .then((data) => {
-          // Redirect to Stripe Checkout
-          return this.stripe.redirectToCheckout({ sessionId: 123 });
-        })
-        .then((res) => {
-          console.log(res);
-        });
-      },
-      getStripePublishableKey() {
-          fetch('http://localhost:4242/config')
-          .then((result) => result.json())
-          .then((data) => {
-              // Initialize Stripe.js
-              this.stripe = Stripe(data.publicKey); // eslint-disable-line no-undef
-          });
-      },
-      created() {
-          this.getStripePublishableKey();
-      },
-      mounted(){
-        const recaptchaScript = document.createElement('script')
-        recaptchaScript.setAttribute('src', 'https://js.stripe.com/v3/')
-        document.head.appendChild(recaptchaScript)
-      }
+    loadUserData() {
+      this.userData = JSON.parse(localStorage.getItem("userData"));
+    },
+    getBalance() {
+      const url = "http://localhost:5002/getBalance/" + this.userData.userID;
+      axios.get(url)
+      .then((response) => {
+        // console.log(response)
+        this.balance = response.data.data.balance
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    },
+    routeNets(route) {
+      this.$router.push({
+        path: '/' + route,
+      });
+    },
+    routeStripe() {
+      const routeData = 'https://buy.stripe.com/test_8wMaGm2Di8m1fXq6oq';
+      window.open(routeData, '_blank');
+
+      const url = "http://localhost:5002/addTen/" + this.userData.userID + "/" + this.userData.balance;
+      axios.put(url, {
+        userID: this.userData.userID,
+        balance: this.userData.balance
+      })
+      .then((response) => {
+        this.userData = JSON.parse(localStorage.getItem("userData"));
+        this.userData.balance = response.data.data
+        localStorage.setItem("userData", JSON.stringify(this.userData));
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    }
+  },
+  mounted() {
+    this.loadUserData()
   }
-})
+});
 </script>
+
+<style>
+</style>
