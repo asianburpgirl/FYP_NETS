a4be199a9d2b73fb04fb238be2832ec35b9d4322
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2023-02-23 03:01:02 +0800

    Update successPage.vue

--- HUAT/src/views/successPage.vue
+++ HUAT/src/views/successPage.vue
@@ -1,10 +1,34 @@
 <template>
-    <ion-page class="ion-padding">
+  <ion-page class="ion-padding">
     <ion-content class="ion-padding">
       <p>Payment has been made. Thank you</p>
+      <ion-button color="primary" routerLink="/tabs/wallet" routerDirection="forward">Back to Wallet</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
-<style>
-</style>
+<script>
+import { 
+  IonButton
+} from '@ionic/vue';
+import { defineComponent } from 'vue'
+  
+export default defineComponent({
+  components: { 
+    IonButton
+  },
+  data() {
+    return {
+      userData: {},
+    }
+  },
+  methods: {
+    loadUserData() {
+      this.userData = JSON.parse(localStorage.getItem("userData"));
+    }
+  },
+  mounted() {
+    this.loadUserData()
+  }
+});
+</script>
