ca2309581294983b6a8e8905f21945e1fb4acea0
LeeShaoMing <90277330+LeeShaoMing@users.noreply.github.com>
2023-02-28 21:25:28 +0800

    Update NETSAmountPage.vue

--- HUAT/src/views/NETSAmountPage.vue
+++ HUAT/src/views/NETSAmountPage.vue
@@ -1,121 +1,108 @@
 <template>
-<ion-page class="ion-padding">
-    <ion-header>
-        <ion-toolbar>
-            <ion-buttons slot="start">
-                <ion-back-button defaultHref="/tabs/wallet"></ion-back-button>
-            </ion-buttons>
-            <ion-title class="ion-text-center">NETS Top-up amount</ion-title>
-        </ion-toolbar>
-    </ion-header>
-    <ion-content class="ion-padding">
-        <img src='assets/images/nets.png' />
-        <ion-button shape="round" expand="block" size="large" @click="routeTen()">$10</ion-button>
-        <ion-button shape="round" expand="block" size="large" @click="routeTwenty()">$20</ion-button>
-        <ion-button shape="round" expand="block" size="large" @click="routeThirty()">$30</ion-button>
-    </ion-content>
-</ion-page>
-</template>
-  
-  
-<script>
-import { 
-    IonButton, 
-    IonPage,
-    IonHeader,
-    IonTitle,
-    IonContent,
-    IonToolbar,
-    IonBackButton,
-} from '@ionic/vue';
-import { defineComponent } from 'vue'
-import axios from "axios";
-  
-export default defineComponent({
-    components: { 
-        IonButton,
-        IonBackButton,
+    <ion-page class="ion-padding">
+        <ion-header>
+            <ion-toolbar>
+                <ion-buttons slot="start">
+                    <ion-back-button defaultHref="/tabs/wallet"></ion-back-button>
+                </ion-buttons>
+                <ion-title class="ion-text-center">NETS Top-up amount</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <img src='assets/images/nets.png' />
+            <ion-button shape="round" expand="block" size="large" @click="routeTen()">$10</ion-button>
+            <ion-button shape="round" expand="block" size="large" @click="routeTwenty()">$20</ion-button>
+            <ion-button shape="round" expand="block" size="large" @click="routeThirty()">$30</ion-button>
+        </ion-content>
+    </ion-page>
+    </template>
+      
+      
+    <script>
+    import { 
+        IonButton, 
         IonPage,
         IonHeader,
         IonTitle,
         IonContent,
         IonToolbar,
-    },
-    data() {
-        return {
-            stripe: null,
-            balance: 0,
-            userData: {}
-      }
-    },
-    methods: {
-        loadUserData() {
-            this.userData = JSON.parse(localStorage.getItem("userData"));
+        IonBackButton,
+    } from '@ionic/vue';
+    import { defineComponent } from 'vue'
+    import axios from "axios";
+      
+    export default defineComponent({
+        components: { 
+            IonButton,
+            IonBackButton,
+            IonPage,
+            IonHeader,
+            IonTitle,
+            IonContent,
+            IonToolbar,
         },
-        routeTen(route) {
-            const url = "http://localhost:5002/addTen/" + this.userData.userID + "/" + parseInt(this.userData.balance);
-            axios.put(url, {
-                userID: this.userData.userID,
-                balance: this.userData.balance
-            })
-            .then((response) => {
+        data() {
+            return {
+                stripe: null,
+                balance: 0,
+                userData: {}
+          }
+        },
+        methods: {
+            loadUserData() {
                 this.userData = JSON.parse(localStorage.getItem("userData"));
-                this.userData.balance = response.data.data
-                localStorage.setItem("userData", JSON.stringify(this.userData));
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
-        
-            this.$router.push({
-                path: '/tabs/wallet',
-            });
+            },
+            routeTen() {
+                const url = "http://localhost:5002/addTen/" + parseInt(this.userData.userID) + "/" + parseInt(this.userData.balance);
+                axios.put(url, {
+                    userID: parseInt(this.userData.userID),
+                    balance: parseInt(this.userData.balance)
+                })
+                .then((response) => {
+                    this.userData = JSON.parse(localStorage.getItem("userData"));
+                    this.userData.balance = response.data.data
+                    localStorage.setItem("userData", JSON.stringify(this.userData));
+                    this.$router.push('/success').then(() => window.location.reload())
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+            },
+            routeTwenty() {
+                const url = "http://localhost:5002/addTwenty/" + parseInt(this.userData.userID) + "/" + parseInt(this.userData.balance);
+                axios.put(url, {
+                    userID: parseInt(this.userData.userID),
+                    balance: parseInt(this.userData.balance)
+                })
+                .then((response) => {
+                    this.userData = JSON.parse(localStorage.getItem("userData"));
+                    this.userData.balance = response.data.data
+                    localStorage.setItem("userData", JSON.stringify(this.userData));
+                    this.$router.push('/success').then(() => window.location.reload())
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+            },
+            routeThirty() {
+                const url = "http://localhost:5002/addThirty/" + parseInt(this.userData.userID) + "/" + parseInt(this.userData.balance);
+                axios.put(url, {
+                    userID: parseInt(this.userData.userID),
+                    balance: parseInt(this.userData.balance)
+                })
+                .then((response) => {
+                    this.userData = JSON.parse(localStorage.getItem("userData"));
+                    this.userData.balance = response.data.data
+                    localStorage.setItem("userData", JSON.stringify(this.userData));
+                    this.$router.push('/success').then(() => window.location.reload())
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+            },
         },
-        routeTwenty() {
-            console.log(this.userData)
-            const url = "http://localhost:5002/addTwenty/" + this.userData.userID + "/" + parseInt(this.userData.balance);
-            axios.put(url, {
-                userID: this.userData.userID,
-                balance: this.userData.balance
-            })
-            .then((response) => {
-                this.userData = JSON.parse(localStorage.getItem("userData"));
-                console.log(this.userData)
-                this.userData.balance = response.data.data
-                localStorage.setItem("userData", JSON.stringify(this.userData));
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
-            
-            
-            this.$router.push({
-                path: '/tabs/wallet',
-            });
-         
-        },
-        routeThirty(route) {
-            const url = "http://localhost:5002/addThirty/" + this.userData.userID + "/" + parseInt(this.userData.balance);
-            axios.put(url, {
-                userID: this.userData.userID,
-                balance: this.userData.balance
-            })
-            .then((response) => {
-                this.userData = JSON.parse(localStorage.getItem("userData"));
-                this.userData.balance = response.data.data
-                localStorage.setItem("userData", JSON.stringify(this.userData));
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
-        
-            this.$router.push({
-                path: '/tabs/wallet',
-            });
-        },
-    },
-    mounted() {
-      this.loadUserData()
-    }
-  });
-  </script>
+        mounted() {
+          this.loadUserData()
+        }
+      });
+      </script>
