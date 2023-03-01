2399e6ef022bd5f1bf169ef3c5de2f35af5ae43f
asianburpgirl <estherrrlychee@gmail.com>
2023-03-01 16:32:55 +0800

    merged branch

--- HUAT/src/views/NETSAmountPage.vue
+++ HUAT/src/views/NETSAmountPage.vue
@@ -1,117 +1,108 @@
 <template>
-<ion-page class="ion-padding">
-    <ion-header>
-        <ion-toolbar>
-            <ion-buttons slot="start">
-                <ion-back-button defaultHref="/tabs/wallet"></ion-back-button>
-            </ion-buttons>
-            <ion-title class="ion-text-center">NETS Top-up amount</ion-title>
-        </ion-toolbar>
-    </ion-header>
-    <ion-content class="ion-padding">
-        <img src='assets/images/nets.png' />
-        <ion-button shape="round" expand="block" size="large" @click="routeTen('success')">$10</ion-button>
-        <ion-button shape="round" expand="block" size="large" @click="routeTwenty('success')">$20</ion-button>
-        <ion-button shape="round" expand="block" size="large" @click="routeThirty('success')">$30</ion-button>
-    </ion-content>
-</ion-page>
-</template>
-  
-  
-<script>
-import { 
-    IonButton, 
-    IonPage,
-    IonHeader,
-    IonTitle,
-    IonContent,
-    IonToolbar,
-    IonBackButton,
-} from '@ionic/vue';
-import { defineComponent } from 'vue'
-import axios from "axios";
-  
-export default defineComponent({
-    components: { 
-        IonButton,
-        IonBackButton,
+    <ion-page class="ion-padding">
+        <ion-header>
+            <ion-toolbar>
+                <ion-buttons slot="start">
+                    <ion-back-button defaultHref="/tabs/wallet"></ion-back-button>
+                </ion-buttons>
+                <ion-title class="ion-text-center">NETS Top-up amount</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <img src='assets/images/nets.png' />
+            <ion-button shape="round" expand="block" size="large" @click="routeTen()">$10</ion-button>
+            <ion-button shape="round" expand="block" size="large" @click="routeTwenty()">$20</ion-button>
+            <ion-button shape="round" expand="block" size="large" @click="routeThirty()">$30</ion-button>
+        </ion-content>
+    </ion-page>
+    </template>
+      
+      
+    <script>
+    import { 
+        IonButton, 
         IonPage,
         IonHeader,
         IonTitle,
         IonContent,
         IonToolbar,
-    },
-    data() {
-        return {
-            stripe: null,
-            balance: 0,
-            userData: {}
-      }
-    },
-    methods: {
-        loadUserData() {
-            this.userData = JSON.parse(localStorage.getItem("userData"));
+        IonBackButton,
+    } from '@ionic/vue';
+    import { defineComponent } from 'vue'
+    import axios from "axios";
+      
+    export default defineComponent({
+        components: { 
+            IonButton,
+            IonBackButton,
+            IonPage,
+            IonHeader,
+            IonTitle,
+            IonContent,
+            IonToolbar,
         },
-        routeTen(route) {
-            const url = "http://localhost:5002/addTen/" + this.userData.userID + "/" + this.userData.balance;
-            axios.put(url, {
-                userID: this.userData.userID,
-                balance: this.userData.balance
-            })
-            .then((response) => {
+        data() {
+            return {
+                stripe: null,
+                balance: 0,
+                userData: {}
+          }
+        },
+        methods: {
+            loadUserData() {
                 this.userData = JSON.parse(localStorage.getItem("userData"));
-                this.userData.balance = response.data.data
-                localStorage.setItem("userData", JSON.stringify(this.userData));
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
-        
-            this.$router.push({
-                path: '/' + route,
-            });
+            },
+            routeTen() {
+                const url = "http://localhost:5002/addTen/" + parseInt(this.userData.userID) + "/" + parseInt(this.userData.balance);
+                axios.put(url, {
+                    userID: parseInt(this.userData.userID),
+                    balance: parseInt(this.userData.balance)
+                })
+                .then((response) => {
+                    this.userData = JSON.parse(localStorage.getItem("userData"));
+                    this.userData.balance = response.data.data
+                    localStorage.setItem("userData", JSON.stringify(this.userData));
+                    this.$router.push('/success').then(() => window.location.reload())
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+            },
+            routeTwenty() {
+                const url = "http://localhost:5002/addTwenty/" + parseInt(this.userData.userID) + "/" + parseInt(this.userData.balance);
+                axios.put(url, {
+                    userID: parseInt(this.userData.userID),
+                    balance: parseInt(this.userData.balance)
+                })
+                .then((response) => {
+                    this.userData = JSON.parse(localStorage.getItem("userData"));
+                    this.userData.balance = response.data.data
+                    localStorage.setItem("userData", JSON.stringify(this.userData));
+                    this.$router.push('/success').then(() => window.location.reload())
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+            },
+            routeThirty() {
+                const url = "http://localhost:5002/addThirty/" + parseInt(this.userData.userID) + "/" + parseInt(this.userData.balance);
+                axios.put(url, {
+                    userID: parseInt(this.userData.userID),
+                    balance: parseInt(this.userData.balance)
+                })
+                .then((response) => {
+                    this.userData = JSON.parse(localStorage.getItem("userData"));
+                    this.userData.balance = response.data.data
+                    localStorage.setItem("userData", JSON.stringify(this.userData));
+                    this.$router.push('/success').then(() => window.location.reload())
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+            },
         },
-        routeTwenty(route) {
-            const url = "http://localhost:5002/addTwenty/" + this.userData.userID + "/" + this.userData.balance;
-            axios.put(url, {
-                userID: this.userData.userID,
-                balance: this.userData.balance
-            })
-            .then((response) => {
-                this.userData = JSON.parse(localStorage.getItem("userData"));
-                this.userData.balance = response.data.data
-                localStorage.setItem("userData", JSON.stringify(this.userData));
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
-        
-            this.$router.push({
-                path: '/' + route,
-            });
-        },
-        routeThirty(route) {
-            const url = "http://localhost:5002/addThirty/" + this.userData.userID + "/" + this.userData.balance;
-            axios.put(url, {
-                userID: this.userData.userID,
-                balance: this.userData.balance
-            })
-            .then((response) => {
-                this.userData = JSON.parse(localStorage.getItem("userData"));
-                this.userData.balance = response.data.data
-                localStorage.setItem("userData", JSON.stringify(this.userData));
-            })
-            .catch((error) => {
-                console.log(error.message);
-            });
-        
-            this.$router.push({
-                path: '/' + route,
-            });
-        },
-    },
-    mounted() {
-      this.loadUserData()
-    }
-  });
-  </script>
+        mounted() {
+          this.loadUserData()
+        }
+      });
+      </script>
--- HUAT/src/views/nearbymePage.vue
+++ HUAT/src/views/nearbymePage.vue
@@ -43,7 +43,7 @@
             placeholder="I am at:"
             @ionChange="
               pushLog($event.detail.value);
-              this.getCarparks();
+              this.getSimulator();
             "
           >
             <IonSelectOption value="orchard">Orchard</IonSelectOption>
@@ -70,15 +70,15 @@
           this.endTime !='' && this.bookingDate != '' ) || this.pageTab=='lotsAvail'
         "
       >
-        <ion-card v-for="carpark in carparksArray" :key="carpark">
-          <ion-img :src="carpark.imagePath"></ion-img>
+        <ion-card v-for="carpark in carparksArraySimu" :key="carpark">
+          <ion-img :src="carpark.image"></ion-img>
 
           <ion-card-header>
-            <ion-card-title>{{ carpark.carparkName }}</ion-card-title>
+            <ion-card-title>{{ carpark.data.carparklocation }}</ion-card-title>
             <ion-card-subtitle></ion-card-subtitle>
-            <ion-card-subtitle>{{ carpark.caparkLocation }}</ion-card-subtitle>
+            <ion-card-subtitle>{{ carpark.data.carparkaddress }}</ion-card-subtitle>
             <h3>
-              <b> {{ carpark.availableLots }}</b> lots available
+              <b> {{ carpark.data.lotbalancehourly }}</b> lots available
             </h3>
             <h3  v-if="pageTab == 'cheapest' && this.startTime != '' &&
           this.endTime !='' && this.bookingDate != '' ">
@@ -208,7 +208,8 @@ export default defineComponent({
   data() {
     return {
       selectedLocation: "",
-      carparksArray: [],
+      // carparksArray: [],
+      carparksArraySimu: [],
       cheapestCarparks: [],
       pageTab: "all",
       dateTimeModal: false,
@@ -245,6 +246,7 @@ export default defineComponent({
 
     },
     confirmDateTime() {
+      console.log("HERE")
       this.setDateTimeOpen(false);
       // const startTimeFormatted = this.startTime.substring(11, 19)
       // const endTimeFormatted = this.endTime.substring(11, 19)
@@ -262,7 +264,7 @@ export default defineComponent({
       axios
         .get(url)
         .then((response) => {
-          this.carparksArray = response.data.data.carparks;
+          this.carparksArraySimu = response.data.data.carparks;
           let fee = 0
           for (const eachCarpark of response.data.data.carparks) {
           // add carpark lots vacacy
@@ -415,9 +417,9 @@ export default defineComponent({
             }
             eachCarpark["totalFee"] = fee
           }
-          console.log(this.carparksArray)
+          console.log(this.carparksArraySimu)
          
-          this.carparksArray.sort(function (a, b) {
+          this.carparksArraySimu.sort(function (a, b) {
             const keyA = a.totalFee;
             const keyB = b.totalFee;
             if (keyA < keyB) return -1;
@@ -425,7 +427,7 @@ export default defineComponent({
             return 0;
           });
 
-          this.carparksArray = this.carparksArray.slice(0, 4);
+          this.carparksArraySimu = this.carparksArraySimu.slice(0, 4);
         })
         .catch((error) => {
           console.log(error.message);
@@ -441,7 +443,7 @@ export default defineComponent({
       if (this.pageTab == "all") {
         this.userOrigin = "";
         this.selectedLocation = "";
-        this.getCarparks();
+        this.getSimulator();
       }
       if (this.pageTab == "lotsAvail") {
         this.userOrigin = "";
@@ -449,15 +451,14 @@ export default defineComponent({
         this.startTime= "",
         this.endTime= "",
         this.bookingDate=  "",
-        this.carparksArray.sort(function (a, b) {
-            const keyA = a.availableLots;
-            const keyB = b.availableLots;
+        this.carparksArraySimu.sort(function (a, b) {
+            const keyA = a.data.lotbalancehourly;
+            const keyB = b.data.lotbalancehourly;
             if (keyA < keyB) return 1;
             if (keyA > keyB) return -1;
             return 0;
           });
-        console.log(this.carparksArray)
-        this.carparksArray = this.carparksArray.slice(0, 4);
+        this.carparksArraySimu = this.carparksArraySimu.slice(0, 4);
 
       }
       
@@ -465,64 +466,66 @@ export default defineComponent({
     pushLog(msg) {
       this.selectedLocation = msg;
     },
-    getCarparks() {
-      this.googleMapDistanceUrl =
-        "https://maps.googleapis.com/maps/api/distancematrix/json?origins=";
-      this.combinedLatLang = "";
+    // getCarparks() {
+    //   this.googleMapDistanceUrl =
+    //     "https://maps.googleapis.com/maps/api/distancematrix/json?origins=";
+    //   this.combinedLatLang = "";
 
-      const url = "http://127.0.0.1:5003/carparks";
-      axios
-        .get(url)
-        .then((response) => {
-          this.carparksArray = response.data.data.carparks;
-          for (const eachCarpark of response.data.data.carparks) {
-            eachCarpark["availableLots"] =
-              eachCarpark.maxCapacity - eachCarpark.currentCapacity;
-            this.combinedLatLang +=
-              eachCarpark.latitude + "," + eachCarpark.longitude + "|";
-          }
+    //   const url = "http://127.0.0.1:5003/carparks";
+    //   axios
+    //     .get(url)
+    //     .then((response) => {
+    //       this.carparksArray = response.data.data.carparks;
+    //       for (const eachCarpark of response.data.data.carparks) {
+    //         eachCarpark["availableLots"] =
+    //           eachCarpark.maxCapacity - eachCarpark.currentCapacity;
+    //         this.combinedLatLang +=
+    //           eachCarpark.latitude + "," + eachCarpark.longitude + "|";
+    //       }
 
-          if (this.selectedLocation == "") {
-            console.log("no location selected");
-          } else {
-            if (this.selectedLocation == "orchard") {
-              this.userOrigin = "1.3064433533620563,103.83276247871694";
-            } else if (this.selectedLocation == "yishun") {
-              this.userOrigin = "1.4304060903894582, 103.83515323243753";
-            } else if (this.selectedLocation == "somerset") {
-              this.userOrigin = "1.3016313961551784, 103.83849995957749";
-            }
+    //       if (this.selectedLocation == "") {
+    //         console.log("no location selected");
+    //       } else {
+    //         if (this.selectedLocation == "orchard") {
+    //           this.userOrigin = "1.3064433533620563,103.83276247871694";
+    //         } else if (this.selectedLocation == "yishun") {
+    //           this.userOrigin = "1.4304060903894582, 103.83515323243753";
+    //         } else if (this.selectedLocation == "somerset") {
+    //           this.userOrigin = "1.3016313961551784, 103.83849995957749";
+    //         }
 
-            this.googleMapDistanceUrl +=
-              this.userOrigin +
-              "&destinations=" +
-              this.combinedLatLang.slice(0, -1) +
-              "&departure_time=now&key=AIzaSyAJXGx7T2ypt5Ew5-9SbDTWF9gqloQUJwI";
-            this.calculateDistance();
-          }
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    },
+    //         this.googleMapDistanceUrl +=
+    //           this.userOrigin +
+    //           "&destinations=" +
+    //           this.combinedLatLang.slice(0, -1) +
+    //           "&departure_time=now&key=AIzaSyAJXGx7T2ypt5Ew5-9SbDTWF9gqloQUJwI";
+    //         this.calculateDistance();
+    //       }
+    //     })
+    //     .catch((error) => {
+    //       console.log(error.message);
+    //     });
+    // },
     calculateDistance() {
       axios
         .get(this.googleMapDistanceUrl)
         .then((response) => {
           const destinations = response.data.rows[0].elements;
 
-          for (let i = 0; i < this.carparksArray.length; i++) {
+          for (let i = 0; i < this.carparksArraySimu.length; i++) {
             // this.carparksArray[i]["distance_km"] = (destinations[i].distance.value / 1000).toPrecision(2)
             // this.carparksArray[i]["duration_mins"] = (destinations[i].duration_in_traffic.value / 60).toPrecision(2)
-            this.carparksArray[i]["distance_km"] =
+            this.carparksArraySimu[i]["distance_km"] =
               destinations[i].distance.text;
-            this.carparksArray[i]["distance_km_value"] =
+            this.carparksArraySimu[i]["distance_km_value"] =
               destinations[i].distance.value;
-            this.carparksArray[i]["duration_mins"] =
+            this.carparksArraySimu[i]["duration_mins"] =
               destinations[i].duration_in_traffic.text;
           }
+          console.log(this.carparksArraySimu)
+          console.log(response)
 
-          this.carparksArray.sort(function (a, b) {
+          this.carparksArraySimu.sort(function (a, b) {
             const keyA = a.distance_km_value;
             const keyB = b.distance_km_value;
             if (keyA < keyB) return -1;
@@ -530,16 +533,216 @@ export default defineComponent({
             return 0;
           });
 
-          this.carparksArray = this.carparksArray.slice(0, 4);
+          this.carparksArraySimu = this.carparksArraySimu.slice(0, 4);
         })
         .catch((error) => {
           console.log(error.message);
         });
     },
+    getSimulator() {
+      this.carparksArraySimu = []
+      this.googleMapDistanceUrl ="https://maps.googleapis.com/maps/api/distancematrix/json?origins=";
+      this.combinedLatLang = "";
+      
+      let url = "http://127.0.0.1:5004/getCarpark/1";
+      axios
+        .post(url,{
+          "requesttype": 1000,
+          "carparkid": 1
+        })
+        .then((response) => {
+          this.carparksArraySimu.push({
+            data :response.data.data,
+            image: "assets/images/paragon.jpg",
+            // lat: 1.3040258775031617,
+            // long: 103.83608284915861
+          })
+          this.combinedLatLang +="1.3040258775031617" + "," + "103.83608284915861" + "|";
+          url = "http://127.0.0.1:5004/getCarpark/2";
+          axios
+            .post(url,{
+              "requesttype": 1000,
+              "carparkid": 2
+            })
+            .then((response) => {
+              this.carparksArraySimu.push({
+                  data :response.data.data,
+                  image: "assets/images/ion.jpg",
+                  // lat: 1.3040258775031617,
+                  // long: 103.83608284915861
+          })
+          this.combinedLatLang +="1.3040258775031617" + "," + "103.83608284915861" + "|";
+              url = "http://127.0.0.1:5004/getCarpark/3";
+              console.log(url)
+              axios
+                .post(url,{
+                  "requesttype": 1000,
+                  "carparkid": 3
+                })
+                .then((response) => {
+                  this.carparksArraySimu.push({
+                    data :response.data.data,
+                    image: "assets/images/takashimaya.jpeg",
+            //         lat: 1.3033454254185042,
+            // long: 103.83455711763565
+          })
+            this.combinedLatLang +="1.3033454254185042" + "," + "103.83455711763565" + "|";
+                  url = "http://127.0.0.1:5004/getCarpark/4";
+                  axios
+                    .post(url,{
+                      "requesttype": 1000,
+                      "carparkid": 4
+                    })
+                    .then((response) => {
+                      this.carparksArraySimu.push({
+                        data :response.data.data,
+                        image: "assets/images/tangs.jpg",
+            //             lat: 1.3040258775031617,
+            // long: 103.83608284915861
+          })
+            this.combinedLatLang +="1.3040258775031617" + "," + "103.83608284915861" + "|";
+                      url = "http://127.0.0.1:5004/getCarpark/5";
+                      axios
+                        .post(url,{
+                          "requesttype": 1000,
+                          "carparkid": 5
+                        })
+                        .then((response) => {
+                          this.carparksArraySimu.push({
+                          data :response.data.data,
+                          image: "assets/images/Wheelock.png",
+            //               lat: 1.3050314731714412,
+            // long: 103.83297614415605
+          })
+            this.combinedLatLang +="1.3050314731714412" + "," + "103.83297614415605" + "|";
+                          url = "http://127.0.0.1:5004/getCarpark/6";
+                          axios
+                            .post(url,{
+                              "requesttype": 1000,
+                              "carparkid": 6
+                            })
+                            .then((response) => {
+                              this.carparksArraySimu.push({
+                              data :response.data.data,
+                              image: "assets/images/313.jpg",
+            //                   lat: 1.301171207812743,
+            // long: 103.8386220085623
+          })
+            this.combinedLatLang +="1.301171207812743" + "," + "103.8386220085623" + "|";
+                              url = "http://127.0.0.1:5004/getCarpark/7";
+                              axios
+                                .post(url,{
+                                  "requesttype": 1000,
+                                  "carparkid": 7
+                                })
+                                .then((response) => {
+                                  this.carparksArraySimu.push({
+                                data :response.data.data,
+                                image: "assets/images/scape.jpg",
+            //                     lat: 1.3010677408660067,
+            // long: 103.83576204980196
+          })
+            this.combinedLatLang +="1.3010677408660067" + "," + "103.83576204980196" + "|";
+                                  url = "http://127.0.0.1:5004/getCarpark/8";
+                                  axios
+                                    .post(url,{
+                                      "requesttype": 1000,
+                                      "carparkid": 8
+                                    })
+                                    .then((response) => {
+                                      this.carparksArraySimu.push({
+                                      data :response.data.data,
+                                      image: "assets/images/wisma.jpeg",
+                                      // lat: 1.303842974291844,
+                                      // long: 103.8333226716273
+                                    })
+                                      this.combinedLatLang +="1.303842974291844" + "," + "103.8333226716273" + "|";
+
+                                      if (this.selectedLocation == "") {
+                                        console.log("no location selected");
+                                      } else {
+                                        if (this.selectedLocation == "orchard") {
+                                          this.userOrigin = "1.3064433533620563,103.83276247871694";
+                                        } else if (this.selectedLocation == "yishun") {
+                                          this.userOrigin = "1.4304060903894582, 103.83515323243753";
+                                        } else if (this.selectedLocation == "somerset") {
+                                          this.userOrigin = "1.3016313961551784, 103.83849995957749";
+                                        }
+
+                                        console.log("SDfa")
+
+                                        console.log(this.carparksArraySimu)
+
+                                      this.googleMapDistanceUrl +=
+                                      this.userOrigin +
+                                      "&destinations=" +
+                                      this.combinedLatLang.slice(0, -1) +
+                                      "&departure_time=now&key=AIzaSyAJXGx7T2ypt5Ew5-9SbDTWF9gqloQUJwI";
+
+                                      console.log(this.googleMapDistanceUrl)
+                                      
+                                      this.calculateDistance();
+                                      }
+                                      
+                                      
+                                    })
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+          
+        })
+
+      // this.calculateDistance()
+
+      // this.googleMapDistanceUrl =
+      //   "https://maps.googleapis.com/maps/api/distancematrix/json?origins=";
+      // this.combinedLatLang = "";
+      
+      // for (const eachCarpark in this.carparksArraySimu){
+      //     console.log(this.carparksArraySimu[eachCarpark])
+      //     this.combinedLatLang +=
+      //         eachCarpark.lat + "," + eachCarpark.long + "|";
+      // }
+
+      //   if (this.selectedLocation == "") {
+      //       console.log("no location selected");
+      //     } else {
+      //       if (this.selectedLocation == "orchard") {
+      //         this.userOrigin = "1.3064433533620563,103.83276247871694";
+      //       } else if (this.selectedLocation == "yishun") {
+      //         this.userOrigin = "1.4304060903894582, 103.83515323243753";
+      //       } else if (this.selectedLocation == "somerset") {
+      //         this.userOrigin = "1.3016313961551784, 103.83849995957749";
+      //       }
+
+      //       this.googleMapDistanceUrl +=
+      //         this.userOrigin +
+      //         "&destinations=" +
+      //         this.combinedLatLang.slice(0, -1) +
+      //         "&departure_time=now&key=AIzaSyAJXGx7T2ypt5Ew5-9SbDTWF9gqloQUJwI";
+      //       this.calculateDistance();
+      //       console.log(this.googleMapDistanceUrl)
+      //     }
+  
+    },
+      // getSimu2(){
+      //   const url2 = "http://127.0.0.1:5004/getCarpark/2";
+      //   console.log(url2)
+      //   axios
+      //     .get(url2)
+      //     .then((response) => {
+      //       console.log(response)
+      //     })
+      // }
   },
 
   mounted() {
-    this.getCarparks();
+    this.getSimulator();
+    
+    // this.getSimu2()
     // this.confirmDateTime();
   },
 });
