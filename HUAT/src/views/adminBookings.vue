77859016c23b1731a72ea16bc7bc94fa7760f2cf
asianburpgirl <estherrrlychee@gmail.com>
2023-03-22 09:17:08 +0800

    Updated list of users and bookings for admin

--- HUAT/src/views/adminBookings.vue
+++ HUAT/src/views/adminBookings.vue
@@ -2,25 +2,72 @@
   <base-layout needToolBar="y" pageTitle="Bookings">
     <ion-grid>
       <h4 class="ion-text-start">List of Bookings</h4>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="3" size-md="3" size-lg="3" class="row-header">Booking Reference</ion-col>
+          <ion-col size="3" size-md="3" size-lg="3" class="row-header">Location</ion-col>
+          <ion-col size="3" size-md="3" size-lg="3" class="row-header">UserID</ion-col>
+          <ion-col size="3" size-md="3" size-lg="3" class="row-header">Status</ion-col>
+          <!-- <ion-col size="3" size-md="3" size-lg="3">Balance</ion-col> -->
+        </ion-row>
+        <ion-row v-for="booking in bookingList" :key="booking.bookingID">
+          <ion-col size="3" size-md="3" size-lg="3">{{booking.bookingRef}}</ion-col>
+          <ion-col size="3" size-md="3" size-lg="3">{{booking.locationName}}</ion-col>
+          <ion-col size="3" size-md="3" size-lg="3">{{booking.userID}}</ion-col>
+          <ion-col size="3" size-md="3" size-lg="3">{{booking.status}}</ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-grid>
   </base-layout>
 </template>
 
 <script>
-import {
-  IonGrid,
-} from "@ionic/vue";
+import { IonGrid, IonRow, IonCol } from "@ionic/vue";
 import { defineComponent } from "vue";
 import axios from "axios";
 export default defineComponent({
   components: {
-    IonGrid,
+    IonGrid, IonRow, IonCol
   },
   data() {
-    return {};
+    return {
+      bookingList: []
+    };
   },
-  methods: {},
+  methods: {
+    getUsers() {
+      const url = "http://127.0.0.1:5001/bookings";
+      axios
+        .get(url)
+        .then((response) => {
+          // console.log(response.data.data);
+          this.bookingList = response.data.data.bookings
+          console.log(this.bookingList)
+          
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    },
+  },
+  mounted(){
+    this.getUsers();
+  }
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+ion-col {
+  background-color: #9cc8fa;
+  border: solid 1px #fff;
+  text-align: center;
+  font-size: 11px;
+}
+
+.row-header {
+  background-color: #4099ff;
+  border: solid 1px #fff;
+  font-weight: 700;
+  text-align: center;
+}
+</style>
